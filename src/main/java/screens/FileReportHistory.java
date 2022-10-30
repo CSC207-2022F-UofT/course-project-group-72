99f333695df9bcc81830eecbacba4edad965eceb
allof02 <petecjh@gmail.com>
2022-10-30 19:43:35 -0400

    Report csv database saver (not implemented)

---
+++ src/main/java/screens/FileReportHistory.java
@@ -0,0 +1,19 @@
+package screens;
+
+
+import use_cases.ReportDsRequestModel;
+import use_cases.reportDsGateway;
+
+import java.io.*;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class FileReportHistory implements reportDsGateway {
+    @Override
+    public void save(ReportDsRequestModel reportdsRequestModel) {
+
+    }
+}
+
