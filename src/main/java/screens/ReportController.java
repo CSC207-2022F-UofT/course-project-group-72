b5b216af486431768104348eae5bb2fa1be04a0d
allof02 <petecjh@gmail.com>
2022-10-30 19:41:30 -0400

    ReportController not finished

--- src/main/java/screens/ReportController.java
+++ src/main/java/screens/ReportController.java
@@ -1,4 +1,20 @@
 package screens;
 
+import use_cases.ReportInteractor;
+import use_cases.ReportRequestModel;
+import use_cases.reportInputBoundary;
+
 public class ReportController {
+
+    final reportInputBoundary input;
+
+    public ReportController(reportInputBoundary input) {
+        this.input = input;
+    }
+
+    public void create(String reason, Review review, User reporter) {
+
+        ReportRequestModel reportRequestModel = new ReportRequestModel(reason, review, reporter);
+        input.Create(reportRequestModel);
+    }
 }
