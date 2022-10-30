82edd6f47ff466b8d4055cffd10b84da58c2adb1
allof02 <petecjh@gmail.com>
2022-10-30 11:47:17 -0400

    Add Report and ReportFactory

---
+++ src/main/java/entities/Report.java
@@ -0,0 +1,45 @@
+package entities;
+
+//Entity layer
+
+//Report Class
+public class Report {
+
+    private final String reason;
+
+    private final Review review;
+
+    private final String reporter_id;
+
+    Report(String reason, Review review, String reporter_id){
+        this.reason = reason;
+        this.review = review;
+        this.reporter_id = reporter_id;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public Review getReview() {
+        return review;
+    }
+
+    public String getReporter_id() {
+        return reporter_id;
+    }
+
+    //later modify get method based on the actual implementation of Review;
+    public User getReportedUser() {
+        return review.getReviewer();
+    }
+
+    //later modify get method based on the actual implementation of Review;
+    public String getReviewContent () {
+        return review.getText();
+    }
+
+
+
+
+}
