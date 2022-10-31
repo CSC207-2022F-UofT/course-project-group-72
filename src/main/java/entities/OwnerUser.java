fde627269208f231a0f432b463b0560eb76748c2
prajogt <tim.prajogi@mail.utoronto.ca>
2022-10-31 15:36:16 -0400

    Updated constructor and getters setters for OwnerRestaurants

--- src/main/java/entities/OwnerUser.java
+++ src/main/java/entities/OwnerUser.java
@@ -5,7 +5,15 @@ import java.util.ArrayList;
 public class OwnerUser extends User{
     private ArrayList<Restaurant> ownedRestaurants = new ArrayList<>();
 
+    public OwnerUser(String username, String password) {
+        super(username, password);
+    }
+
     public void addRestaurant(Restaurant restaurant) {
         this.ownedRestaurants.add(restaurant);
+    }
+
+    public ArrayList<Restaurant> getOwnedRestaurants() {
+        return this.ownedRestaurants;
     }
 }
