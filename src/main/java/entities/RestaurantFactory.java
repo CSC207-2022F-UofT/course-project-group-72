9443084196ab268a9e542420e1f837b1908b435e
prajogt <tim.prajogi@mail.utoronto.ca>
2022-10-31 15:53:20 -0400

    Updated create method for the RestaurantFactory

--- src/main/java/entities/RestaurantFactory.java
+++ src/main/java/entities/RestaurantFactory.java
@@ -1,5 +1,7 @@
 package entities;
 
-public interface RestaurantFactory {
-    Restaurant create(OwnerUser owner, String name, String location, String cuisineType, int priceBucket);
+public class RestaurantFactory {
+    Restaurant create(OwnerUser owner, String name, String location, String cuisineType, int priceBucket) {
+        return new Restaurant(owner, name, location, cuisineType, priceBucket);
+    }
 }
