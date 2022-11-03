d1a104bd52fc1c1738a9a8b5b13b8c9311e9eb26
Timothius Prajogi <tim.prajogi@mail.utoronto.ca>
2022-11-03 18:51:44 -0400

    Extracted class into RestaurantAttributes to prevent bloating in the Restaurant class. Allows for all sortable attributes to be accessed in one class.

--- src/main/java/entities/Restaurant.java
+++ src/main/java/entities/Restaurant.java
@@ -5,34 +5,26 @@ import java.util.ArrayList;
 public class Restaurant {
     private String name;
     private String location;
-    private String cuisineType;
-    private int priceBucket;
-    private double AvgStars = 0;
     private ArrayList<Review> reviews = new ArrayList<Review>();
     private OwnerUser owner;
+    private RestaurantAttributes attributes = new RestaurantAttributes();
 
 
     //Constructors
     public Restaurant(OwnerUser owner, String name, String location, String cuisineType, int priceBucket) {
         this.name = name;
         this.location = location;
-        this.cuisineType = cuisineType;
-        this.priceBucket = priceBucket;
         this.owner = owner;
+        this.attributes.setPriceBucket(priceBucket);
+        this.attributes.setCuisineType(cuisineType);
     }
 
     //Setters
     public void setName(String name) {this.name = name;}
 
-    public void setCuisineType(String cuisineType) {this.cuisineType = cuisineType;}
-
-    public void setPriceBucket(int priceBucket) {this.priceBucket = priceBucket;}
-
-    public void setLocation(String location) {this.location = location;}
-
     public void addReview(Review review) {
         this.reviews.add(review);
-        this.AvgStars = ((double)review.getStars() + this.AvgStars) / (this.reviews.size() + 1);
+        this.attributes.addReview(review.getStars());
     }
 
     //Getters
@@ -40,11 +32,7 @@ public class Restaurant {
 
     public String getLocation() {return this.location;}
 
-    public int getPriceBucket() {return this.priceBucket;}
-
-    public String getCuisineType() {return this.cuisineType;}
-
-    public double getAvgStars() {return this.AvgStars;}
-
     public ArrayList<Review> getReviews() {return this.reviews;}
+
+    public RestaurantAttributes getAttributes() {return this.attributes;}
 }
