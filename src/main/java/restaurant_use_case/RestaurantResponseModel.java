7503777f16e9334cb8d3156f8fa3ea8bdbed121c
Timothius Prajogi <tim.prajogi@mail.utoronto.ca>
2022-11-07 00:02:59 -0500

    Interactors and response model update - createRestaurant completed - editRestaurant to be done when database complete

--- src/main/java/restaurant_use_case/RestaurantResponseModel.java
+++ src/main/java/restaurant_use_case/RestaurantResponseModel.java
@@ -1,4 +1,32 @@
 package restaurant_use_case;
 
+import entities.Restaurant;
+
 public class RestaurantResponseModel {
+
+    Restaurant restaurant;
+    String creationTime;
+    String operation;
+
+    public RestaurantResponseModel(Restaurant restaurant, String creationTime, String operation) {
+        this.restaurant = restaurant;
+        this.creationTime = creationTime;
+        this.operation = operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public String getCreationTime() {
+        return creationTime;
+    }
 }
--- src/main/java/restaurant_use_case/editRestaurant.java
+++ src/main/java/restaurant_use_case/editRestaurant.java
@@ -1,6 +1,23 @@
 package restaurant_use_case;
 
+import entities.RestaurantFactory;
+import restaurant_screens.RestaurantPresenter;
+
 public class editRestaurant implements RestaurantInputBoundary{
+    private final RestaurantDSGateway gateway;
+    private final RestaurantPresenter presenter;
+
+    public editRestaurant(RestaurantDSGateway dataGateway,
+                          RestaurantPresenter presenter) {
+        gateway = dataGateway;
+        this.presenter = presenter;
+    }
+
     @Override
-    public RestaurantResponseModel create(RestaurantRequestModel requestModel) {}
+    public RestaurantResponseModel create(RestaurantRequestModel requestModel) {
+        if (!gateway.existsByLocation(requestModel.getName())) {
+            return presenter.prepareFailView("RESTAURANT DOES NOT EXIST");
+        }
+        //TODO Overwrite restaurant using gateway
+    }
 }
