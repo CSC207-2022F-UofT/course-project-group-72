b0f83e065f3df95f76faecd6b446c6373fc430c1
HENRY KLINCK <henry.klinck@mail.utoronto.ca>
2022-11-08 11:06:03 -0500

    Implement banned_user and num_reports of User and associated methods.

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -6,11 +6,15 @@ public class User {
     private String username;
     private String password;
     private ArrayList<Review> past_reviews;
+    private Boolean banned_user;
+    private int num_reports;
 
     public User(String username, String password) {
         this.username = username;
         this.password = password;
         this.past_reviews = new ArrayList<>();
+        this.banned_user = false;
+        this.num_reports = 0;
     }
     // Get & Set Username
     public String getUsername() {
@@ -37,4 +41,14 @@ public class User {
     public void add_review(Review review) {
         this.past_reviews.add(review);
     }
+
+    // Get & Set Banned User
+    public Boolean getBanned_user() { return this.banned_user; }
+
+    public void banUser() { this.banned_user = true; }
+
+    // Get & Add to Number of Reports of User
+    public int getNum_reports() { return this.num_reports; }
+
+    public void addOneReport() { this.num_reports += 1; }
 }
