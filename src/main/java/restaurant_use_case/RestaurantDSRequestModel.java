8450c5e7db7fb7ca8600c9325172196bf7aa34af
Timothius Prajogi <tim.prajogi@mail.utoronto.ca>
2022-11-07 21:27:24 -0500

    First implementation of FileRestaurant, this version rewrites the whole file, which may be updated later

--- src/main/java/restaurant_use_case/RestaurantDSRequestModel.java
+++ src/main/java/restaurant_use_case/RestaurantDSRequestModel.java
@@ -2,12 +2,16 @@ package restaurant_use_case;
 
 import entities.OwnerUser;
 
+import java.util.ArrayList;
+
 public class RestaurantDSRequestModel {
     private String name;
     private String location;
     private String cuisineType;
     private int priceBucket;
     private String owner;
+    private int avgStars;
+    private ArrayList<String> reviews;
 
     public RestaurantDSRequestModel(String owner, String name, String location,
                                   String cuisineType, int priceBucket) {
@@ -16,6 +20,20 @@ public class RestaurantDSRequestModel {
         this.owner = owner;
         this.cuisineType = cuisineType;
         this.priceBucket= priceBucket;
+        this.avgStars = 0;
+        this.reviews = new ArrayList<>();
+    }
+
+    public RestaurantDSRequestModel(String owner, String name, String location,
+                                    String cuisineType, int priceBucket, int avgStars,
+                                    ArrayList<String> reviews) {
+        this.name = name;
+        this.location = location;
+        this.owner = owner;
+        this.cuisineType = cuisineType;
+        this.priceBucket= priceBucket;
+        this.avgStars = avgStars;
+        this.reviews = reviews;
     }
 
     // Retrieve Information
@@ -24,4 +42,6 @@ public class RestaurantDSRequestModel {
     String getCuisineType() {return this.cuisineType;}
     int getPriceBucket() {return this.priceBucket;}
     String getOwner() {return this.owner;}
+    int getAvgStars() {return this.avgStars;}
+    ArrayList<String> getReviews() {return this.reviews;}
 }
