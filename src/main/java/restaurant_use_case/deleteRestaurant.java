0b2bdb9975171d5320292159128fdf4abf0a009b
Timothius Prajogi <tim.prajogi@mail.utoronto.ca>
2022-11-15 11:35:50 -0500

    Updates to deleteRestaurant use case and GUI - created a DeleteRequestModel and added owner to check for ownership

--- src/main/java/restaurant_use_case/deleteRestaurant.java
+++ src/main/java/restaurant_use_case/deleteRestaurant.java
@@ -3,6 +3,7 @@ package restaurant_use_case;
 import restaurant_screens.RestaurantDeletePresenter;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class deleteRestaurant implements RestaurantDeleteInputBoundary{
     private final RestaurantDSGateway gateway;
@@ -14,18 +15,19 @@ public class deleteRestaurant implements RestaurantDeleteInputBoundary{
     }
 
     @Override
-    public RestaurantResponseModel delete(String location) {
-        if (!gateway.existsByLocation(location)) {
+    public RestaurantResponseModel delete(RestaurantDeleteRequestModel requestModel) {
+        if (!gateway.existsByLocation(requestModel.getRestaurant().getLocation())) {
             // Checks if the restaurant is already in the system and can be edited
             return presenter.prepareFailView("RESTAURANT DOES NOT EXIST");
+        } else if (!Objects.equals(requestModel.getRestaurant().getOwnerID(), requestModel.getOwner().getUsername())){
+            // Checks if the restaurant is owned by the current user
+            return presenter.prepareFailView("You do not own this restaurant");
         }
 
         LocalDateTime now = LocalDateTime.now();
         //reinitializing restaurant here is necessary may be changed later
-        String restaurant = gateway.retrieveRestaurant(location).getName();
-        gateway.deleteRestaurant(location);
-
-        return presenter.prepareSuccessView("deleted", restaurant, now.toString());
+        gateway.deleteRestaurant(requestModel.getRestaurant().getLocation());
+        return presenter.prepareSuccessView("deleted", requestModel.getRestaurant().getName(), now.toString());
 
     }
 }
