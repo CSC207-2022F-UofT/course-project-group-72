2ac66ae1f728e73cbacdc4e354e4686bc2d646c1
woods <j2hashz0p@outlook.com>
2022-11-21 15:00:00 -0500

    Commited Response Formatter, forgot to include in previous commit. This prepares the success/fail screens.

---
+++ src/main/java/restaurant_screens/ChoicesResponseFormatter.java
@@ -0,0 +1,17 @@
+package restaurant_screens;
+
+import restaurant_use_case.ChoicesResponseModel;
+
+public class ChoicesResponseFormatter implements ChoicesPresenter{
+
+    @Override
+    public ChoicesResponseModel prepareSuccessView(ChoicesResponseModel responseModel){
+        // TODO: success screen
+
+        return responseModel;
+
+    }
+
+    // TODO: prepareFailView
+
+}
