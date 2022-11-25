e4c12048d8c03853cee39d468d82544292e7472f
Allof02 <70244801+Allof02@users.noreply.github.com>
2022-11-25 13:20:32 -0500

    fix gradle

--- src/test/java/ReportTest.java
+++ src/test/java/ReportTest.java
@@ -12,7 +12,7 @@ import entities.Review;
 import entities.User;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 
 import java.io.File;
