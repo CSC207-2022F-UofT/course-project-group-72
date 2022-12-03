d16581da4f8c19da7af89bccedff4ad48acdf431
Timothius Prajogi <tim.prajogi@mail.utoronto.ca>
2022-12-02 23:37:08 -0500

    Screen tests; incomplete

---
+++ src/test/java/restaurant_tests/screens/DeleteViewTest.java
@@ -0,0 +1,34 @@
+package restaurant_tests.screens;
+
+import org.junit.Test;
+import restaurant_use_case.interactors.FileRestaurant;
+import restaurant_use_case.screens.DeleteRestaurantView;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
+import static org.junit.Assert.fail;
+
+public class DeleteViewTest {
+    DeleteRestaurantView deleteView = new DeleteRestaurantView(
+            null,
+            null,
+            new FileRestaurant("src/test/java/restaurant_tests/temptest.csv"),
+            null,
+            null
+            );
+
+    public DeleteViewTest() throws IOException {
+    }
+
+    @Test
+    public void actionPerformedTest() {
+        ActionEvent event = new ActionEvent("Confirm", 0, "Confirm");
+
+        try {
+            deleteView.actionPerformed(event);
+        } catch (Exception ignored) {
+            fail();
+        }
+    }
+}
