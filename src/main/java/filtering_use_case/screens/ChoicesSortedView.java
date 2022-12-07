50d7dd15234c5edbde280bbdb0def1463c1d948a
jr <58833849+foreverwoods@users.noreply.github.com>
2022-12-07 00:24:15 -0500

    Added Jarrod's Implementation of Back and Home buttons

--- src/main/java/filtering_use_case/screens/ChoicesSortedView.java
+++ src/main/java/filtering_use_case/screens/ChoicesSortedView.java
@@ -14,7 +14,7 @@ import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class ChoicesSortedView extends JFrame implements IFrame{
+public class ChoicesSortedView extends JFrame implements IFrame, ActionListener{
     //TODO: java documentation
     JLabel restaurantName;
     JLabel restaurantPrice;
@@ -34,6 +34,29 @@ public class ChoicesSortedView extends JFrame implements IFrame{
         this.user = user;
 
         //TODO: organize/format components
+
+        // Create the back and home buttons (Uniform Implementation)
+        JButton backButton = new JButton("Back");
+        backButton.setFont(backButton.getFont().deriveFont(12F));
+        backButton.setOpaque(true);
+        backButton.addActionListener(this);
+
+        JButton homeButton = new JButton("Home");
+        homeButton.setFont(homeButton.getFont().deriveFont(12F));
+        homeButton.setOpaque(true);
+        homeButton.addActionListener(this);
+
+        // Create a panel to hold the back and home buttons, then add those buttons (Uniform Implementation)
+        JPanel IPanel = new JPanel();
+        IPanel.setLayout(new BoxLayout(IPanel, BoxLayout.X_AXIS));
+        IPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        IPanel.setVisible(true);
+        IPanel.add(backButton);
+        IPanel.add(homeButton);
+
+        this.add(IPanel);
+
+        // Setup Layout for Restaurant List
         setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
 
         // Loop through sorted restaurants list
@@ -60,15 +83,13 @@ public class ChoicesSortedView extends JFrame implements IFrame{
             viewRestaurantButton.addActionListener(new ViewRestaurantActionListener(this, user, restaurant));
 
             this.add(restaurantPanel);
-
-
         }
 
         // Window options
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        this.pack();
         this.setLocationRelativeTo(null);
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
         this.setVisible(true);
         this.repaint();
 
@@ -94,6 +115,16 @@ public class ChoicesSortedView extends JFrame implements IFrame{
             this.dispose();
         }catch(IOException e){
             JOptionPane.showMessageDialog(this, "An error occurred. Please try again later.");
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // Action Listeners for Back and Home Buttons (Uniform Implementation)
+        if (e.getActionCommand().equals("Back")) {
+            this.back();
+        } else if (e.getActionCommand().equals("Home")) {
+            this.home();
         }
     }
 }
